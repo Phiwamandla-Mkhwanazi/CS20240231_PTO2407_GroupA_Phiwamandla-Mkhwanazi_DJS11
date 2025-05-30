fe764ccb28a633a2970af066ceb64ef4b33efd8e
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-30 16:54:40 +0200

    add comments to main application file outlining route configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
 // App.jsx
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+// Importing React Router components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Importing layout and pages
 import Layout from "./layouts/Layout";
 import Home from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import About from "./pages/About";
 import Privacy from './pages/Privacy';
 import Guide from './pages/Guide';
-import NotFound from "./pages/NotFound"; 
-
-
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
+    // BrowserRouter enables routing using the HTML5 history API
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Layout />}>
+        {/* Main layout wrapper for all nested routes */}
+        <Route path="/" element={<Layout />}>
+          {/* Index route (i.e., homepage) */}
           <Route index element={<Home />} />
+
+          {/* Route for search page */}
           <Route path="search" element={<SearchPage />} />
+
+          {/* Route for about page */}
           <Route path="about" element={<About />} />
+
+          {/* Route for privacy policy page */}
           <Route path="privacy" element={<Privacy />} />
+
+          {/* Route for usage guide or instructions */}
           <Route path="guide" element={<Guide />} />
-          <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+
+          {/* Catch-all route for any undefined paths */}
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
